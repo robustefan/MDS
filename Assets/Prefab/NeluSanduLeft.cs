ee5745ab0ce2fe630ca72cab25085d6271580641
robustefan <robustefan12@gmail.com>
2018-03-16 22:23:51 +0200

    Movement Script
    Am adaugat Movement Script pentru caracterul nostru, pe ambele parti ale sforii.
    
    Am mai adaugat 2 imagini pentru platforme si 2 prefaburi pentru acestea.

---
+++ Assets/Prefab/NeluSanduLeft.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class NeluSanduLeft : MonoBehaviour
+{
+
+	public GameObject a;
+
+
+	void Update()
+	{
+		if (Input.GetKeyDown ("up"))
+			transform.Translate (0, 0.6f, 0);
+		if (Input.GetKeyDown ("down"))
+			transform.Translate (0, -0.6f, 0);
+		if (Input.GetKeyDown ("left"))
+		{
+			Destroy (gameObject);
+			Instantiate (a, new Vector3 ( this.transform.position.x - 0.8f , this.transform.position.y , 0) , Quaternion.identity);
+		}
+
+	}
+
+}
